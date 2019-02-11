091d4183072bd533d27565b633853a327418a982
dannyAmiouni <37172573+dannyAmiouni@users.noreply.github.com>
2019-02-10 21:33:19 -0500

    Assignment 4

---
+++ JavascriptAs4/src/app/underline.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appUnderline]'
+})
+export class UnderlineDirective {
+  @Input () deco: string;
+  constructor(private elm: ElementRef) { }
+  @HostListener ('mouseover') onMouseOver() {
+    this.check(this.deco);
+  }
+  @HostListener ('mouseout') onMouseOut() {
+    this.check('none');
+  }
+  private check(style) {
+    this.elm.nativeElement.style.textDecoration = style;
+  }
+}
