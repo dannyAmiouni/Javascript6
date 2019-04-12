8b816c0788d4d6298ab31bf78f3ad9977db243b8
dannyAmiouni <37172573+dannyAmiouni@users.noreply.github.com>
2019-04-12 00:51:13 -0400

    Update content.service.ts

--- JavascriptAs4/src/app/services/content.service.ts
+++ JavascriptAs4/src/app/services/content.service.ts
@@ -10,10 +10,28 @@ import { MessageService} from '../message.service';
 })
 export class ContentService {
 
-    constructor(private messageService: MessageService) { }
-    getContentObs(): Observable<Content[]>{
-        this.messageService.add('Content retrieved!');
-        return of (CONTENT);
+    constructor(private messageService: MessageService) {
     }
-}
 
+    getContentObs(): Observable<Content[]> {
+        this.messageService.add('Content retrieved!');
+        return of(CONTENT);
+    }
+
+    addContent(content: Content): Observable<Content> {
+        let thePromise = new Promise((success, fail)) => {
+            let testPass = true;
+            if (testPass){
+                success(this.messageService.add('Content was added'));
+            }
+            else{
+                fail(this.messageService.add('Content failed to be added'))
+            }
+            thePromise.then(successResult => console.log(successResult))
+                .then(failResult => console.log(failResult));
+        }
+        return this.http.post<Content>("api/content", content, this.httpOptions);
+    }
+
+
+}
